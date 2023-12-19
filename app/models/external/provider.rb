754568a821f94c42b1db59fbfd5439febe426415
Ollie Treend <ollie@treend.uk>
2023-12-19 15:56:05 +0000

    Retrieve all providers, not just the first page
    This commit shifts things around a little bit. The main change is that
    paginated API responses are now handled gracefully.
    
    `External::Provider.all` will now pull _all_ providers from the API.
    
    Caching is also done at a slightly higher level, to avoid needing to
    cache each page request seperately.

--- app/models/external/provider.rb
+++ app/models/external/provider.rb
@@ -1,7 +1,10 @@
 class External::Provider
-  attr_reader :name, :email, :website, :ukprn
+  ATTRIBUTES = %i[code name email website ukprn]
 
-  def initialize(attributes)
+  attr_reader *ATTRIBUTES
+
+  def initialize(code, attributes)
+    @code = code
     @name = attributes["name"]
     @email = attributes["email"]
     @website = attributes["website"]
@@ -9,17 +12,37 @@ class External::Provider
   end
 
   def self.all
-    api_response = TeacherTrainingCoursesApi.get("/recruitment_cycles/2023/providers")
-    api_response.fetch("data").map { |provider| new(provider["attributes"]) }
+    providers_from_api.map { |code, attributes| new(code, attributes) }
   end
 
-  def self.find(ukprn)
-    self.all.detect { |p| p.ukprn == ukprn } or raise ProviderNotFound.new(ukprn)
+  def self.find(code)
+    if attributes = providers_from_api[code]
+      new(code, attributes)
+    else
+      raise ProviderNotFound.new(code)
+    end
   end
 
   class ProviderNotFound < StandardError
-    def initialize(ukprn)
-      super("Couldn't find provider with UKPRN '#{ukprn}'")
+    def initialize(code)
+      super("Couldn't find provider with code '#{code}'")
+    end
+  end
+
+private
+
+  def self.providers_from_api
+    @providers_from_api ||= Rails.cache.fetch("providers_from_api", expires_in: 1.hour) do
+      # Fetch all providers from the API
+      providers = TeacherTrainingCoursesApi.get_all_pages("/recruitment_cycles/2023/providers")
+
+      # Drop attributes we don't need
+      providers = providers.map do |provider|
+        provider["attributes"].select { |key, _value| ATTRIBUTES.include?(key.to_sym) }
+      end
+
+      # Transform to a Hash indexed by provider code
+      providers.index_by { |provider| provider["code"] }
     end
   end
 end
--- app/services/teacher_training_courses_api.rb
+++ app/services/teacher_training_courses_api.rb
@@ -2,29 +2,34 @@ require 'net/http'
 
 class TeacherTrainingCoursesApi
   BASE_URL = "https://api.publish-teacher-training-courses.service.gov.uk/api/public/v1"
-  CACHE_REQUESTS_FOR = 1.hour
 
   def self.get(endpoint)
     request(method: :get, endpoint:)
   end
 
+  def self.get_all_pages(endpoint, per_page: 500)
+    results = []
+    is_last_page = false
+
+    until is_last_page
+      response = request(method: :get, endpoint:)
+      results.concat response.fetch("data")
+
+      if next_page = response.dig("links", "next")
+        endpoint = next_page.delete_prefix(BASE_URL)
+      else
+        is_last_page = true
+      end
+    end
+
+    results
+  end
+
 private
 
   def self.request(method:, endpoint:)
-    key = cache_key(method:, endpoint:)
-    response = Rails.cache.fetch(key, expires_in: CACHE_REQUESTS_FOR) do
-      Net::HTTP.public_send method, uri(endpoint)
-    end
+    url = URI("#{BASE_URL}#{endpoint}")
+    response = Net::HTTP.public_send(method, url)
     JSON.parse(response)
   end
-
-  def self.cache_key(method:, endpoint:)
-    endpoint_hash = Digest::MD5.hexdigest(endpoint)
-    "#{name}_#{method}_#{endpoint_hash}"
-  end
-
-  def self.uri(endpoint)
-    URI("#{BASE_URL}/#{endpoint}")
-  end
-
 end
